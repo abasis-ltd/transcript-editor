a078beff90a0fc2e8513bc11188261865bf84a7c
foglabs <foglaboratories@gmail.com>
2023-11-02 14:23:16 -0400

    'add kevins updated bash script for releasing from fixit to aapb, handling ams2 style guids'

--- scripts/shellScriptToDuplicateAndPushNewFilesToS3.sh
+++ scripts/shellScriptToDuplicateAndPushNewFilesToS3.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # created for WGBH for use the its Media Library and Archives
-# by Kevin Carter, 2019-07-16
+# by Kevin Carter, 2023-11-02
 
 export endpointURL='https://s3.us-east-1.amazonaws.com' ;
 export bucketName='americanarchive.org' ;
 
-export profileName='transcript-editor-script'   # personalize this for your own use to avoid subsequent dialogs/prompts for the profile;
+export profileName=''   # personalize this for your own use to avoid subsequent dialogs/prompts for the profile;
 export profileNameList=$(grep '\[' "$HOME/.aws/config" | tr -d '\[\]') ;
 export profileNameCount=$(echo "$profileNameList" | wc -l | tr -dC '[0-9]') ;
+
 
 function usage {
 clear ;
@@ -23,7 +24,7 @@ echo "\$(output lines of valid paths to readable files) | $(basename "$0") [ 2> 
 echo ;
 echo ' - or - ' ;
 echo ;
-echo '2)     invoke with arguments' ;
+echo '2)     invoke with arguments:' ;
 echo "$(basename "$0") \$(output valid paths to readable files)  [ 2> /path/to/error/file ] ";
 echo ;
 echo "******* NOTE:  *******"
@@ -39,24 +40,27 @@ exit
 
 function awsCopyPush {
 arg=$1;
-		echo "processing $arg"
+		echo "processing $arg" 
 		errString='' ;
 				# Checking to ensure a filename was specified and that it exists
 		if [ ! -f "$arg" -o ! -r "$arg" ]; then
 			echo "ERROR: $arg is not a valid path or file is unreadable" >&2 ;
 			continue ;
-		fi
+		fi 
 		fileName=$(basename "$arg");
-		guid=$(echo "$fileName" | cut -f1-4 -d \- ) ;
-
-				# sanity checks for GUID-ness, but does crash if you have an extra hyphen in your path.
-		if [ "$(echo "$fileName" | tr -dC '-' | wc -c | tr -dC '[0-9]')" != 4 ] ; then
-			errString=$(echo "$errString '$arg' file name must have 5 hyphenated parts") ;
+		guid=$(echo "$fileName" | sed 's#^.*cpb-aacip.#cpb-aacip-#1;s#[^[:alnum:]-].*##1' );
+		
+				# sanity checks for GUID-ness
+# 		if [ "$(echo "$fileName" | tr -dC '-' | wc -c | tr -dC '[0-9]')" != 4 ] ; then 
+# 			errString=$(echo "$errString '$arg' file name must have 5 hyphenated parts") ;		
+# 		fi
+# 		if [ "$(echo "$fileName" | cut -f1 -d \- )" != 'cpb' -o "$(echo "$fileName" | cut -f2 -d \- )" != 'aacip' -o ! -z "$(echo "$fileName" | cut -f3 -d \- | tr -d '[0-9]')" -o "$(echo "$fileName" | cut -f5 -d \- )" != 'transcript.json' ] ; then 
+# 			errString=$(echo "$errString '$arg'"' file name hypenated part(s) must resemble "cpb-aacip-[:digit:]-[:alnum:]-transcript.json"') ;		
+# 		fi
+		if [ "$fileName" != "$guid"'-transcript.json' ]; then
+			errString=$(echo "$errString '$arg' file name should be '$guid-transcript.json' for use by the AAPB site");
 		fi
-		if [ "$(echo "$fileName" | cut -f1 -d \- )" != 'cpb' -o "$(echo "$fileName" | cut -f2 -d \- )" != 'aacip' -o ! -z "$(echo "$fileName" | cut -f3 -d \- | tr -d '[0-9]')" -o "$(echo "$fileName" | cut -f5 -d \- )" != 'transcript.json' ] ; then
-			errString=$(echo "$errString '$arg'"' file name hypenated part(s) must resemble "cpb-aacip-[:digit:]-[:alnum:]-transcript.json"') ;
-		fi
-		if [ -z "$errString" ]; then
+		if [ -z "$errString" ]; then 
 			echo "checking for previous copies" ;
 			copyVersion=$(aws s3api list-objects --profile "$profileName" --bucket "$bucketName" --prefix "transcripts/ORIGINAL-$guid/" --query 'Contents[].Key' | grep -c \" | tr -dC '[0-9]') ;
 			if [ "$copyVersion" == 0 ] ; then
@@ -78,21 +82,21 @@ if [ -p /dev/stdin ]; then
 #        echo "Data was piped to this script!"
         # If we want to read the input line by line
         if [ "$profileNameCount" -lt 2 ]
-        then
+        then 
 			export profileName='default' ;
 			echo "using default profile" ;
-        elif [ -z "$profileName" ] ; then
+        elif [ -z "$profileName" ] ; then 
 			echo "Click dialog window to choose an access profile"
 			profileName=$(osascript <<EOF
 copy paragraphs of "$profileNameList" to profileList
 choose from list profileList with prompt "Choose a profile name " with title "S3 Profile Options" default items (item 1 of profileList)
 EOF
 );
-			if [ "$profileName" == 'false' ] ; then
-				exit
+			if [ "$profileName" == 'false' ] ; then 
+				exit 
 			fi
 			echo "using profile '$profileName'"
-		fi
+		fi 
 		while IFS= read line; do
 #			echo "Line: ${line}"
 			awsCopyPush $line ;
@@ -101,7 +105,7 @@ EOF
 elif [ "$#" -ne 0 ]; then
 #		echo "No input was found on stdin, skipping!"
 	if [ "$profileNameCount" -lt 2 ]
-	then
+	then 
 		export profileName='default' ;
 		echo "using default profile" ;
 	elif [ -z "$profileName" ]; then
@@ -112,7 +116,7 @@ elif [ "$#" -ne 0 ]; then
 			echo "$profileNameList" | cat -n
 			read profileNum;
 			sanity=$(echo "$profileNameList" | head -$profileNum 2>/dev/null | tail -1 2>/dev/null | cut -f2-) ;
-			if [ "$sanity" -a ! -z "$(seq 1 $profileNameCount | grep $profileNum)" ]
+			if [ "$sanity" -a ! -z "$(seq 1 $profileNameCount | grep $profileNum)" ] 
 			then
 			export profileName=$sanity ;
 			fi ;
@@ -129,4 +133,8 @@ else
 fi
 
 exit ;
+
+
+
+
 
